ce0ebb20fdeb47d287b1aaf3a21764f3c27f00db
James Robert Perih <james@hotdang.ca>
2025-11-23 14:36:45 -0600

    Merge meta tags from old site.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,35 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Four And A Half Giraffes",
-  description: "Standing tall to help small Canadian businesses with websites, apps, & automation.",
+  title: "Four and a Half Giraffes Ltd. | We craft Internet-powered Customer Experiences",
+  description: "Four And A Half Giraffes Ltd. is responsible for carefully placing independent parts in just the right places, to craft the perfect Internet-powered Customer Experience.",
+  metadataBase: new URL("https://fourandahalfgiraffes.ca"),
+  openGraph: {
+    title: "Four And A Half Giraffes Ltd. | Crafting Internet-powered Customer Experiences",
+    description: "Four And A Half Giraffes Ltd. is responsible for carefully placing independent parts in just the right places, to craft the perfect Internet-powered Customer Experience.",
+    url: "https://fourandahalfgiraffes.ca",
+    siteName: "Four And A Half Giraffes Ltd.",
+    type: "website",
+    locale: "en_CA",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@4point5giraffes",
+    creator: "@4point5giraffes",
+    title: "Four And A Half Giraffes Ltd. | Crafting Internet-powered Customer Experiences",
+    description: "Four And A Half Giraffes Ltd. is responsible for carefully placing independent parts in just the right places, to craft the perfect Internet-powered Customer Experience.",
+  },
+  icons: {
+    icon: "/favicon.png",
+    apple: "/apple-touch-icon.png",
+    other: [
+      {
+        rel: "mask-icon",
+        url: "/safari-pinned-tab.svg",
+        color: "#5bbad5",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
